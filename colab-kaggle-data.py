d33b92c4d7b5f9a3b843ee1f9af306ff882188f0
Shea Lambert <6855717+SheaML@users.noreply.github.com>
2018-12-20 19:20:40 -0500

    Create colab-kaggle-data.py
    lines for downloading kaggle datasets to google colab

---
+++ colab-kaggle-data.py
@@ -0,0 +1,7 @@
+# must upload kaggle.json API token first. available through Kaggle 'my account'
+!pip install -U -q kaggle
+!mkdir -p ~/.kaggle
+!cp kaggle.json ~/.kaggle/
+
+#uncomment and download your data:
+#!kaggle competitions download
